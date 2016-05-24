15f84799026ed033153af52429fc1f46ba593cdf
Boris shishov <borisshishov@gmail.com>
2016-05-24 16:18:57 +0300

    New reading

--- src/static/js/tests/reading.js
+++ src/static/js/tests/reading.js
@@ -1,6 +1,10 @@
-var text = "Выросла на краю поляны Крапива. Поднялась над травами и смутилась. Цветы вокруг красивые и душистые, ягоды вкусные.  Ни вкуса приятного, ни яркого цвета, ни сладкого запаха у неё нет! \
-— Будто меня и не было совсем, будто я и не жила! Пропади пропадом такое крапивное счастье!";
+var startSpeed = 5;
+var endSpeed = 15;
+var startPosition = 1000;
 
+var text = "Немногим более 50 лет прошло с тех пор, как появилась первая электронная вычислительная машина. За этот короткий для развития общества период сменилось несколько поколений вычислительных машин, а первые ЭВМ сегодня являются музейной редкостью. Сама история развития вычислительной техники представляет немалый интерес, показывая тесную взаимосвязь математики с физикой (прежде всего с физикой твердого тела, полупроводников, электроникой) и современной технологией, уровнем развития которой во многом определяется прогресс в производстве средств вычислительной техники.";
+
+var isAnimating = false;
 var textObject = draw.plain(text).move(1000, '50%')
 textObject.font({
     family: 'Georgia',
@@ -8,12 +12,41 @@ textObject.font({
     anchor: 'left',
     leading: 1,
 });
-//textObject.rebuild(false);
+
 var width = textObject.length();
+
+var progress = function() {
+    return Math.abs(textObject.x() - startPosition) / (width + startPosition);
+}
+
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                          window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+
+var move = function() {
+    var p = progress();
+    var speed = startSpeed + p * (endSpeed - startSpeed);
+
+    textObject.dx(-speed);
+    if(p > 1.0) {
+        complete();
+    }
+};
+
+var complete = function() {
+    isAnimating = false;
+    test.complete();
+}
+
+var anim = function(timestamp) {
+    if(isAnimating) {
+        move();
+        requestAnimationFrame(anim);
+    }
+}
 
 test.on("start", function() {
     textObject.move(1000, '50%');
-    textObject.animate('10s').x(-width + 100).after(function() {
-        test.complete();
-    });
+    requestAnimationFrame(anim);
+    isAnimating = true;
 });
